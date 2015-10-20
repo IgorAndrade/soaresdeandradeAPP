123a8ba3780fb323b9776fddf1a1f6887d2cabb6
Igor Andrade <andrade.igor@gmail.com>
2015-10-20 13:31:35 -0200

    primeiro commit

---
+++ www/js/config.js
@@ -0,0 +1,13 @@
+var cfg = {
+    email:{
+        contato: {
+            to: "andrade.igor@gmail.com",
+            subject: "assunto",
+            body: 'body',
+            isHtml: true
+            }
+    }
+    
+    
+    
+};
