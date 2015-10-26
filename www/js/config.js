59989f0572247e20273505863fe106fc394e605f
Igor Andrade <andrade.igor@gmail.com>
2015-10-26 21:57:19 -0200

    add fotos

--- www/js/config.js
+++ www/js/config.js
@@ -12,9 +12,12 @@ var cfg = {
         contato2:"55 (21) 2215-4993"
     },
     fotos:[
-        {titulo:"Dra. Maria Rita Soares de Andrade",img:"http://arquivos.integrawebsites.com.br/12983/8cada1978368fe114541507a732c8874.jpg"},
-        {titulo:"Ministro Ricardo Lewandowski",img:"http://arquivos.integrawebsites.com.br/12983/0a9809ef86078a9b5c6d233b12739acf.jpg"},
-        {titulo:"Presidente da OAB/RJ dr",img:"http://arquivos.integrawebsites.com.br/12983/8127cf2715b33a31c77704656892db25.jpg"}
+        {titulo:"Dra. Maria Rita Soares de Andrade",img:"img/fotos/foto1.jpg"},
+        {titulo:"Ministro Ricardo Lewandowski",img:"img/fotos/foto2.jpg"},
+        {titulo:"Presidente da OAB/RJ dr",img:"img/fotos/foto3.jpg"},
+        {titulo:"Dr. Felipe Santa Cruz, Preside ...",img:"img/fotos/foto4.jpg"},
+        {titulo:"Evento na OAB/RJ",img:"img/fotos/foto5.jpg"},
+        {titulo:"Os melhores do ano",img:"img/fotos/foto6.jpg"}
     ]
     
     
